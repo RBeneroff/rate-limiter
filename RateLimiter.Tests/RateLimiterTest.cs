ca3bd51ee3bbcf88dcd045587109c2a5e4b3f32e
Rachel Beneroff <rachel@joylabhq.com>
2025-04-18 14:59:44 -0700

    update comments

--- RateLimiter.Tests/RateLimiterTest.cs
+++ RateLimiter.Tests/RateLimiterTest.cs
@@ -16,7 +16,7 @@ namespace RateLimiter.Tests
             string resourceId = "api/no-rule";
 
             // should allow all requests when no rule is configured
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 12; i++)
             {
                 Assert.That(rateLimiter.IsRequestAllowed(clientToken, resourceId), Is.True,
                     $"Request {i+1} should be allowed when no rule is configured");
@@ -34,7 +34,6 @@ namespace RateLimiter.Tests
             string clientToken = "test-client";
             string resourceId = "api/test";
 
-            // should follow the configured rule's behavior
             Assert.That(rateLimiter.IsRequestAllowed(clientToken, resourceId), Is.True, "First request should be allowed");
             Assert.That(rateLimiter.IsRequestAllowed(clientToken, resourceId), Is.True, "Second request should be allowed");
             Assert.That(rateLimiter.IsRequestAllowed(clientToken, resourceId), Is.False, "Third request should be denied");
@@ -51,11 +50,11 @@ namespace RateLimiter.Tests
 
             string clientToken = "test-client";
 
-            // resource 1 should allow 1 request
+            // should allow 1 request
             Assert.That(rateLimiter.IsRequestAllowed(clientToken, "api/resource1"), Is.True, "First request to resource1 should be allowed");
             Assert.That(rateLimiter.IsRequestAllowed(clientToken, "api/resource1"), Is.False, "Second request to resource1 should be denied");
 
-            // resource 2 should allow 2 requests
+            // should allow 2 requests
             Assert.That(rateLimiter.IsRequestAllowed(clientToken, "api/resource2"), Is.True, "First request to resource2 should be allowed");
             Assert.That(rateLimiter.IsRequestAllowed(clientToken, "api/resource2"), Is.True, "Second request to resource2 should be allowed");
             Assert.That(rateLimiter.IsRequestAllowed(clientToken, "api/resource2"), Is.False, "Third request to resource2 should be denied");
